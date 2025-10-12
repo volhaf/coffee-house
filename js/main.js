b3f7a9e456a7ec163ceb893061622ec65398ecd3
volhaf <Volha.f@outlook.com>
2025-10-12 23:56:01 +0200

    feat: add alert

---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+    alert("Dear colleagues, the project is still in progress. Due to being on sick leave, I haven’t managed to finish it yet. I would be very grateful if you could review and assess my work in 2–3 days. Thank you, colleagues, for your understanding.");
+});
