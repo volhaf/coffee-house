8606fe7f6c23d76534228f808d7c9434721a6cb0
volhaf <Volha.f@outlook.com>
2025-10-15 00:54:50 +0200

    fix: fix controls

--- js/slider.js
+++ js/slider.js
@@ -10,21 +10,26 @@ const controls = document.querySelectorAll('.favorite__controls-btn');
 
 const slidesArray = Array.from(slides); // сделали массив из 3 элементов
 let currentIndex = 0; //первый слайд
-let slideWidth = slidesArray[0].clientWidth;≠
+let slideWidth = slidesArray[0].clientWidth;
+let autoSlideInterval; // интервал
+
+
 
 const showSlide = (index) =>{
     sliderRow.style.transform = `translateX(-${index * slideWidth}px)`;
 
-    controls.forEach((btn, i) => {
-        if (i === index) {
-            btn.classList.add('favorite__controls-btn--active');
-        } else {
-            btn.classList.remove('favorite__controls-btn--active');
-        }
-    });
+    controls.forEach(btn => btn.classList.remove('favorite__controls-btn--active'));
+    controls[index].classList.add('favorite__controls-btn--active');
 } //двигаем
 
 showSlide(currentIndex); //начало
+
+setInterval(() => {
+    currentIndex++;
+    if (currentIndex >= slides.length) currentIndex = 0;
+    showSlide(currentIndex);
+}, 4000);
+
 
     prevBtn.addEventListener('click', () => {
         currentIndex--;
