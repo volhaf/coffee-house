a7d7dfcfe34cd5b4fe27527034a3c425a0768cd7
volhaf <Volha.f@outlook.com>
2025-10-15 15:57:58 +0200

    feat: add object menu

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const buttons = document.querySelectorAll('.menu__tabs-btn');
+const productsMenu = document.querySelector('.section__menu-grid');
+
+const menu= {
+    coffee: [],
+    tea: [],
+    dessert: []
+};
