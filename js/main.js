4a69e321bb708cdede446906ee342b0480b76748
volhaf <Volha.f@outlook.com>
2025-10-15 12:55:40 +0200

    feat: add new alert

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-    alert("Dear colleagues, the project is still in progress. Due to being on sick leave, I haven’t managed to finish it yet. I would be very grateful if you could review and assess my work in 2–3 days. Thank you, colleagues, for your understanding.");
+    alert("The project is mostly completed, but the filtering and the modal window on the second page have not been implemented yet.");
 });
