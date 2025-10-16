c7c0ab838eefa13c1906001a9bdc919c48251c79
volhaf <Volha.f@outlook.com>
2025-10-16 15:46:32 +0200

    feat: remove alert

--- js/main.js
+++ js/main.js
@@ -1,3 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    alert("The project is mostly completed, but the filtering and the modal window on the second page have not been implemented yet.");
-});
+'use strict';
